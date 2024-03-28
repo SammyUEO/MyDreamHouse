57ce6e4f410cad73fcd997b83ccc1ca05ca423bf
SammyUEO <samuel.pop@emanuel.ro>
2024-03-28 20:57:27 +0200

    casa

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function setup() {
 function draw() {
   background(220);
 
-  // Desenare perete
+  // perete
   fill(255);
   rect(100, 200, 200, 150);
 
-  // Desenare acoperiș
+  // acoperiș
   fill('red');
   triangle(100, 200, 300, 200, 200, 100);
 
-  // Desenare ușă
+  // ușă
   fill('brown');
   rect(170, 270, 60, 80);
 
-  // Desenare fereastră
+  // ferestre
   fill('cyan');
   rect(250, 240, 50, 50);
 
